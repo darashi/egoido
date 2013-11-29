bd1bec1e15f6d1290151a5b16d2c8a2dffd4ca4a
Yoji Shidara <dara@shidara.net>
2013-11-30 01:30:31 +0900

    Autolink tweets

--- renderer.js
+++ renderer.js
@@ -1,7 +1,20 @@
 var jade = require('jade');
 var fs = require('fs');
+var twitter = require('twitter-text');
 
 var renderer = jade.compile(fs.readFileSync(__dirname + '/template.jade'));
+
+var autolink = function(tweet) {
+  var entities = tweet.entities;
+  var urlEntities = entities.urls;
+  if (entities.media) {
+    urlEntities = urlEntities.concat(entities.media);
+  }
+  var html = twitter.autoLink(tweet.text, {
+    urlEntities: urlEntities
+  });
+  return html;
+}
 
 module.exports.renderTweet = function(tweet) {
   var screen_name = tweet.user.screen_name;
@@ -10,7 +23,8 @@ module.exports.renderTweet = function(tweet) {
     url: {
       tweet: 'https://twitter.com/' + screen_name + '/status/' + tweet.id_str,
       user: 'https://twitter.com/' + screen_name
-    }
+    },
+    html: autolink(tweet)
   };
   var html = renderer(locals);
 
